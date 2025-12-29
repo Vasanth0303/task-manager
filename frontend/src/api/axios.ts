96775baa0d2d683ab4cc60609257e8b122baa04f
Your Name <your.email@example.com>
2025-12-29 17:19:50 +0530

    update frontend api url for render

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://task-manager-1-twv4.onrender.com/api",
 });
 
-API.interceptors.request.use(config => {
+API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
